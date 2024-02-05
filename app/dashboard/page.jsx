358d02b8216292ab05b3ea7d09b105cd4d174da5
Ridwan <ridwandev.219@gmail.com>
2024-02-05 17:07:09 +0700

    update

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
-import styles from '../ui/dashboard/dashboard.module.css'
+import React from "react";
+import styles from "../ui/dashboard/dashboard.module.css";
+import Card from "../ui/dashboard/card/Card";
+import Rightbar from "../ui/dashboard/rightbar/Rightbar";
+import Transaction from "../ui/dashboard/transaction/Transaction";
+import Chart from "../ui/dashboard/chart/Chart";
 
 export default function page() {
   return (
-    <div></div>
-  )
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transaction/>
+        <Chart/>
+      </div>
+      <div className={styles.side}> 
+        <Rightbar/>
+      </div>
+    </div>
+  );
 }
---
+++ app/ui/dashboard/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './footer.module.css'
+
+export default function Footer() {
+  return (
+    <div className={styles.container}>
+        <div className={styles.logo}>Ridwan Dev</div>
+        <div className={styles.text}>© All rights reserved.</div>
+    </div>
+  )
+}
